8eff396ada919bd8e4fe9bede7b7ec6237d0a322
Raydone <b.milliet@bucci-industries.com>
2022-08-18 08:24:10 +0200

    display/hide form

--- client-app/src/features/activities/form/ActivityForm.svelte
+++ client-app/src/features/activities/form/ActivityForm.svelte
@@ -1,22 +1,49 @@
 <script>
     import {Button, Card, Form, Input} from "sveltestrap";
-    import {activityEditMode} from "../../../app/layout/stores/ActivityStores.js";
+    import {activityEditMode, selectedActivity} from "../../../app/layout/stores/ActivityStores.js";
     
     function cancelFormClicked(){
         activityEditMode.set(false);
     }
+    
+    let activityForm = {
+        id: "",
+        title: "",
+        description: "",
+        category: "",
+        date: "",
+        city: "",
+        venue: ""
+    };
+    
+    function setActivityEmptyForCreate()
+    {
+        activityForm.id = "";
+        activityForm.title = "";
+        activityForm.description = "";
+        activityForm.category = "";
+        activityForm.date = "";
+        activityForm.city = "";
+        activityForm.venue = "";
+    }
+    
+    $:if ($selectedActivity)
+        activityForm = $selectedActivity;
+    else
+        setActivityEmptyForCreate();
+    
 </script>
 
 <Card>
     <Form class="p-2">
-        <Input class="my-2" placeholder="Title"/>
-        <Input class="my-2" placeholder="Description" type="textarea"/>
-        <Input class="my-2" placeholder="Category"/>
-        <Input class="my-2" placeholder="Date"/>
-        <Input class="my-2" placeholder="City"/>
-        <Input class="my-2" placeholder="Venue"/>
+        <Input class="my-2" placeholder="Title" bind:value={activityForm.title}/>
+        <Input class="my-2" placeholder="Description" type="textarea" bind:value={activityForm.description}/>
+        <Input class="my-2" placeholder="Category" bind:value={activityForm.category}/>
+        <Input class="my-2" placeholder="Date" bind:value={activityForm.date}/>
+        <Input class="my-2" placeholder="City" bind:value={activityForm.city}/>
+        <Input class="my-2" placeholder="Venue" bind:value={activityForm.venue}/>
         <div class="col text-right pr-0">
-            <Button on:click={cancelFormClicked} class="mr-1" color="secondary" type="submit">Cancel</Button>
+            <Button on:click={cancelFormClicked} class="mr-1" color="secondary">Cancel</Button>
             <Button class="ml-1" color="success" type="submit">Submit</Button>
         </div>
     </Form>
