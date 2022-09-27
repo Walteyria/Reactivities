b0d19d2f4832e2464a54b7b884330c255fce7e19
Raydone <b.milliet@bucci-industries.com>
2022-09-27 09:19:47 +0200

    remove unused url

--- client-app/src/app/layout/App.svelte
+++ client-app/src/app/layout/App.svelte
@@ -4,13 +4,12 @@
   import ActivityForm from "../../features/activities/form/ActivityForm.svelte";
   import NavBar from "./NavBar.svelte";
   import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard.svelte";
-  export let url = undefined;
 </script>
 
 <main>
   <NavBar/>
   <div class="container-sm">
-    <Router url={url}>
+    <Router>
       <Route path="/" component={HomePage} />
       <Route path="/activities" component={ActivityDashboard} />
       <Route path="/createActivity" component={ActivityForm} />
